7c2f81250cfa54dd851780c09219bceef9622cc1
Ivan Dzhantov <ivan.dzhantov@a1.bg>
2019-05-10 16:58:09 +0300

    Section Two, Video Three

--- app/src/main/java/com/vankata/testappcourse/MainActivity.kt
+++ app/src/main/java/com/vankata/testappcourse/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.vankata.testappcourse
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -17,6 +18,12 @@ class MainActivity : AppCompatActivity() {
 
             Toast.makeText(this, "Button was clicked !", Toast.LENGTH_SHORT).show()
         }
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
